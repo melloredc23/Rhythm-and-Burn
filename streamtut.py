19f1fb0d344791c083513a0982a157f371dc4fe7
Kendall Andrews <78447160+melloredc23@users.noreply.github.com>
2024-10-18 13:00:44 -0400

    Update streamtut.py

--- streamtut.py
+++ streamtut.py
@@ -23,10 +23,10 @@ st.header("Welcome to my Streamlit app!")
 
 #Overview
 #st.markdown("This app shows if there is a correlation between the tempo and health benefits between different dance styles to determine which dance style is better for physical fitness and health.")
-#df = pd.read_csv('/Users/kendallandrews/Downloads/dances/dance data.csv', encoding='latin-1')
-df = pd.read_csv('dance data.csv')
-df2 = pd.read_csv('exercise_dataset.csv')
-#df2 = pd.read_csv("/Users/kendallandrews/Downloads/calories-burned-during-exercise-and-activities/exercise_dataset.csv")
+df = pd.read_csv('/Users/kendallandrews/Downloads/dances/dance data.csv', encoding='latin-1')
+#df = pd.read_csv('dance data.csv')
+#df2 = pd.read_csv('exercise_dataset.csv')
+df2 = pd.read_csv("/Users/kendallandrews/Downloads/calories-burned-during-exercise-and-activities/exercise_dataset.csv")
 d3 = df2.drop(['130 lb', '155 lb', '180 lb', '205 lb'], axis=1)
 d3 = d3.iloc[[29, 34, 35, 36],:]
 
@@ -57,9 +57,9 @@ if section == 'Data Overview':
         #os.listdir(data_dir)
         #encoding='latin-1' - helps solve the error: UnicodeDecodeError: 'utf-8' codec can't decode byte 0xf1 in position 41293: invalid continuation byte
 
-        #df = pd.read_csv('/Users/kendallandrews/Downloads/dances/dance data.csv', encoding='latin-1')
+        df = pd.read_csv('/Users/kendallandrews/Downloads/dances/dance data.csv', encoding='latin-1')
         #df = pd.read_csv('/Users/kendallandrews/Downloads/dances/dance data.csv')
-        df= pd.read_csv('dance data.csv')
+        #df= pd.read_csv('dance data.csv')
 
         st.write("Dance Styles and Genres Dataset")
         st.dataframe(df)
@@ -153,8 +153,8 @@ print(z1)
         #od.download(dataset2, force=True)
         #data_dir2 = './calories-burned-during-exercise-and-activities'
         #os.listdir(data_dir2)
-        df2= pd.read_csv('exercise_dataset.csv')
-        #df2 = pd.read_csv("/Users/kendallandrews/Downloads/calories-burned-during-exercise-and-activities/exercise_dataset.csv")
+        #df2= pd.read_csv('exercise_dataset.csv')
+        df2 = pd.read_csv("/Users/kendallandrews/Downloads/calories-burned-during-exercise-and-activities/exercise_dataset.csv")
     
         st.write("Calories Dataset")
         st.dataframe(df2)
@@ -178,7 +178,7 @@ print(z1)
 
 #Correlation Analysis
 if section == 'Correlation Analysis':
-    df = pd.read_csv('dance data.csv')
+    df = pd.read_csv('/Users/kendallandrews/Downloads/dances/dance data.csv', encoding='latin-1')
     st.header("Correlation Analysis Between Tempo and Health Benefits")
     #df["Health Benefits"]
     df["health_count"] = df["Health Benefits"].str.split(",").apply(len)
@@ -214,7 +214,7 @@ if section == 'Correlation Analysis':
 # Outliers - bar plots
 if section == 'Visualizations':
     #df = pd.read_csv('/Users/kendallandrews/Downloads/dances/dance data.csv', encoding='latin-1')
-    df= pd.read_csv('dance data.csv')
+    df = pd.read_csv('/Users/kendallandrews/Downloads/dances/dance data.csv', encoding='latin-1')
     st.header("Visualizations")
 
     #df["Health Benefits"]
